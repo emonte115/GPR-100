4b65813f329965998c3d77e71676af123ce838b9
emonte115 <elliot.montesanto@mymail.champlain.edu>
2020-02-21 16:52:18 -0500

    Added framework

--- GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.cpp
+++ GPRO Framework/source/GPRO-FW-Launcher/GPRO-FW-Launcher_main.cpp
@@ -1,7 +1,12 @@
 // GPRO-FW-Launcher.cpp : This file contains the 'main' function. Program execution begins and ends there.
 //
 
+#include <stdio.h>
+#include <iomanip>
+
 int main()
 {
+	int test = foo(1);
 
+	cout << test;
 }
